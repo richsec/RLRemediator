ad4c678e373f091e492f1883a917e6ad39f875a2
Hao Wu <haowu@Haos-MacBook-Pro.local>
2016-09-14 15:55:10 -0700

    script for simulating the connection attempts to w close and w open

---
+++ src/main/scripts/connection_tcp_layer.py
@@ -0,0 +1,44 @@
+import socket
+
+from time import sleep
+
+HOST_W_OPEN = '54.208.220.41'
+HOST_W_CLOSE = '52.45.56.227'
+
+num_conn = 100
+
+
+def make_tcp_connections(host, port, num_conn):
+    for i in range(0, num_conn):
+        try:
+            sleep(0.1)
+            print '#%d establishing TCP connection to %s:%s ...' % (
+                i, host, port)
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((host, port))
+            print '--- connection established'
+            # close right after the connection
+            # such connection is hard to distinguish
+            # from the rejected TCP connection
+            s.close()
+            print '--- connection closed'
+        except Exception as e:
+            print 'Failed to connection to %s:%s, error %s' % (host, port, e)
+
+# port 80
+make_tcp_connections(HOST_W_OPEN, 80, num_conn)
+
+# port 443
+make_tcp_connections(HOST_W_OPEN, 443, num_conn)
+
+# port 22
+make_tcp_connections(HOST_W_OPEN, 22, num_conn)
+
+# port 80
+make_tcp_connections(HOST_W_CLOSE, 80, num_conn)
+
+# port 443
+make_tcp_connections(HOST_W_CLOSE, 443, num_conn)
+
+# port 22
+make_tcp_connections(HOST_W_CLOSE, 22, num_conn)
