00c7054b083f86c6b3852a8a271f825581ee94aa
Hao Wu <hao@redlock.io>
2016-09-19 11:03:17 -0700

    some changes for running script

--- src/main/scripts/connection_tcp_layer.py
+++ src/main/scripts/connection_tcp_layer.py
@@ -1,6 +1,7 @@
 import socket
 
 from time import sleep
+from random import randint
 
 HOST_W_OPEN = '54.208.220.41'
 HOST_W_CLOSE = '52.45.56.227'
@@ -9,12 +10,15 @@ num_conn = 100
 
 
 def make_tcp_connections(host, port, num_conn):
+    # src_port = randint(50000, 59999)
     for i in range(0, num_conn):
         try:
             sleep(0.1)
             print '#%d establishing TCP connection to %s:%s ...' % (
                 i, host, port)
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            # s.bind(('', src_port))
+            print '--- binded the port'
             s.connect((host, port))
             print '--- connection established'
             # close right after the connection
@@ -25,14 +29,14 @@ def make_tcp_connections(host, port, num_conn):
         except Exception as e:
             print 'Failed to connection to %s:%s, error %s' % (host, port, e)
 
-# port 80
-make_tcp_connections(HOST_W_OPEN, 80, num_conn)
+# # port 80
+# make_tcp_connections(HOST_W_OPEN, 80, num_conn)
 
-# port 443
-make_tcp_connections(HOST_W_OPEN, 443, num_conn)
+# # port 443
+# make_tcp_connections(HOST_W_OPEN, 443, num_conn)
 
-# port 22
-make_tcp_connections(HOST_W_OPEN, 22, num_conn)
+# # port 22
+# make_tcp_connections(HOST_W_OPEN, 22, num_conn)
 
 # port 80
 make_tcp_connections(HOST_W_CLOSE, 80, num_conn)
